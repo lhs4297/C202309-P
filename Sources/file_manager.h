be2e0a57c524a60215fc16413b4e2069059a72ea
lhys4297 <leehys4297@gmail.com>
2023-11-19 17:39:26 +0900

    main틀,file_manager미완

---
+++ Sources/file_manager.h
@@ -0,0 +1,27 @@
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
+
+#define MAX_FILES 100   // 파일 최대 개수
+#define MAX_PATH_LENGTH 64   // 파일 경로의 최대 길이
+#define MAX_CONTENT_LENGTH 1024   // 파일 내용 최대 길이
+
+typedef struct {   // 파일 내용 저장하기 위해 필요한 곳
+    char path[MAX_PATH_LENGTH];
+    char extension[10];
+    time_t created;
+    long size;
+    char content[MAX_CONTENT_LENGTH];
+    char title[MAX_PATH_LENGTH];
+} FileInfo;
+
+typedef struct {   // FileInfo 구조체 배열과 파일 개수를 저장하기 위한 멤버로 구성
+    FileInfo files[MAX_FILES];
+    int count;
+} FileManager;
+
+
+// 함수들 초기화, 파일 정보 수집
+void init_file_manager(FileManager* file_manager);
+void collect_file_info(FileManager* file_manager);
+
+#endif
---
+++ Sources/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "file_manager.h"
+#include "rule_manager.h"
+#include "history_manager.h"
+
+int main(){
+
+    //---------관리 시스템----------
+
+    // file 관리자 불러옴
+
+    // rule 관리자 불러옴 
+
+    // history 관리자 불러옴
+
+    //------------데이터 시스템--------------
+
+    //파일 목록 수집, 정보 분석
+
+    // 분류 기준 설정 및 처리
+
+    // 파일 분류 및 이동
+
+    // 히스토리 저장
+
+    return 0;
+}
+    
